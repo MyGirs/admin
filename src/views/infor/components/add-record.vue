ea98603bf20a4ba14277f77977fab6cc49dd0222
zxpipixia <1770366430@qq.com>
2024-04-10 19:00:33 +0800

    feat: 表格提交

---
+++ src/views/infor/components/add-record.vue
@@ -0,0 +1,41 @@
+<template>
+  <el-dialog v-model="addDialog" title="新增" width="800">
+    <el-form :model="form" label-width="auto">
+      <el-form-item label="时间">
+        <el-date-picker v-model="form.time" type="date" placeholder="请选择" />
+      </el-form-item>
+      <el-form-item label="天气">
+        <el-input v-model="form.weather" />
+      </el-form-item>
+      <el-form-item label="值班领导">
+        <el-input v-model="form.user" />
+      </el-form-item>
+      <el-form-item label="路线">
+        <el-input v-model="form.route" />
+      </el-form-item>
+      <el-form-item label="内容">
+        <el-input v-model="form.content" type="textarea" :rows="2" />
+      </el-form-item>
+      <el-form-item label="处理措施">
+        <el-input v-model="form.handlingMeasures" type="textarea" :rows="2" />
+      </el-form-item>
+    </el-form>
+    <template #footer>
+      <div class="dialog-footer">
+        <el-button @click="addDialog = false">取消</el-button>
+        <el-button type="primary" @click="dialogFormVisible = false"> 提交 </el-button>
+      </div>
+    </template>
+  </el-dialog>
+</template>
+<script setup>
+import { ElButton, ElDialog, ElForm, ElInput, ElFormItem, ElDatePicker } from 'element-plus'
+import { ref } from 'vue'
+const addDialog = defineModel()
+
+const form = ref({
+  name: null
+})
+</script>
+
+<style lang="less" scoped></style>
--- src/views/infor/duty-record.vue
+++ src/views/infor/duty-record.vue
@@ -1,6 +1,75 @@
 <template>
-  <div class="record">
-    <h1>记录表</h1>
+  <div class="search-warp">
+    <el-button type="primary" @click="handleAdd">新增</el-button>
   </div>
+  <ContentWrap title="值班记录表">
+    <Table
+      v-model:pageSize="pageSize"
+      v-model:currentPage="currentPage"
+      :columns="columns"
+      :data="dataList"
+      :loading="loading"
+      :pagination="
+        canShowPagination
+          ? {
+              total: total
+            }
+          : undefined
+      "
+    />
+  </ContentWrap>
+  <AddRecord v-model="addDialog" />
 </template>
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ElButton } from 'element-plus'
+import { ContentWrap } from '@/components/ContentWrap'
+import { Table, TableColumn } from '@/components/Table'
+import AddRecord from './components/add-record.vue'
+import { ref } from 'vue'
+
+const loading = ref(false)
+const pageSize = ref(10)
+const currentPage = ref(1)
+const total = ref(0)
+const addDialog = ref(false)
+
+const columns: TableColumn[] = [
+  {
+    field: 'time',
+    label: '时间'
+  },
+  {
+    field: 'weather',
+    label: '天气'
+  },
+  {
+    field: 'user',
+    label: '值班领导'
+  },
+  {
+    field: 'route',
+    label: '路线'
+  },
+  {
+    field: 'content',
+    label: '内容'
+  },
+  {
+    field: 'handlingMeasures',
+    label: '处理措施'
+  }
+]
+let dataList = ref([])
+const canShowPagination = ref(true)
+
+const handleAdd = () => {
+  addDialog.value = true
+}
+</script>
+<style lang="less" scoped>
+.search-warp {
+  margin-bottom: 10px;
+  display: flex;
+  justify-content: flex-end;
+}
+</style>
